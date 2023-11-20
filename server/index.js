018ddd38318e53642eff20b321c25667f1934561
Ali <alidrl26@gmail.com>
2023-11-20 08:49:20 +0200

    index.js

--- server/index.js
+++ server/index.js
@@ -1,1 +1,24 @@
-let x = 5
+'use strict'
+
+const express = require('express')
+const app = express()
+
+
+require('dotenv').config()
+const PORT = process.env.PORT || 8000
+const HOST = process.env.HOST || '127.0.0.1'
+
+
+
+app.all('/', (req,res) => {
+  res.send({
+    error:false,
+    message:'Welcome to Blog App server !!!'
+  })
+})
+
+
+
+
+
+app.listen(PORT , HOST, ()=> console.log(`http://${HOST}:${PORT}`))
---
+++ server/readme.md
@@ -0,0 +1,1 @@
+## npm i express dotenv express-async-errors mongoose cors
